0ce073027e4368053c484a196706dddaca7d0f38
qTronor <romankuzmenko73q@gmail.com>
2022-02-23 20:36:53 +0300

    Added tasks 1-2

---
+++ PoP_Tasks/Task1/Task1.py
@@ -0,0 +1,11 @@
+import math
+
+
+def main(y, z):
+    ans = (pow(y, 2) / 70) + (pow(math.sqrt(z + 57 + pow(y, 2)), 5) / 98) - ((
+            66 * pow(76 * pow(z, 3) + pow(y, 2), 4) + 99 * pow(z, 2)) / (
+            pow(math.acos(y - 1 - 26 * pow(z, 2)), 2)))
+    return ans
+
+
+main(0.94, 0.19)
